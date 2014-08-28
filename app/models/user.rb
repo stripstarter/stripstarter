37a30cef9db074ef6458b4f7390df82dbfe777e3
Stripstarter <stripstarter@gmail.com>
2014-08-27 22:36:55 -0700

    Travis fix attempt #2

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,20 @@ class User < ActiveRecord::Base
 
   acts_as_authentic
 
-  # Travis hack
+  ###############
+  # Travis hack #
+  ###############
+  
   attr_writer :password, :password_confirmation
+
+  validates :password, :presence => true,
+                       :confirmation => true,
+                       :length => {:within => 6..40},
+                       :on => :create
+  validates :password, :confirmation => true,
+                       :length => {:within => 6..40},
+                       :allow_blank => true,
+                       :on => :update
 
   ################
   # Associations #
