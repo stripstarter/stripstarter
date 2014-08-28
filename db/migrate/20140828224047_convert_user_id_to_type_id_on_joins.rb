63a4bccb61a1e1a7ddbf743a1ca8ee68655f6608
Stripstarter <stripstarter@gmail.com>
2014-08-28 16:11:23 -0700

    Pledger and Performer factories up and humming

--- db/migrate/20140828224047_convert_user_id_to_type_id_on_joins.rb
+++
@@ -1,5 +0,0 @@
-class ConvertUserIdToTypeIdOnJoins < ActiveRecord::Migration
-  def change
-    # Oops
-  end
-end
--- spec/factories/admin.rb
+++ spec/factories/admin.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
-  # Do not use this factory unless you really mean to
-  factory :user do
+  factory :admin do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
     password { "password" }
     password_confirmation { "password" }
+    type { "Admin" }
   end
 end
