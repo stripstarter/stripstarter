0788a8d99440387670ac40e52d336a9005ac97a6
Stripstarter <stripstarter@gmail.com>
2014-08-29 16:36:13 -0700

    Updated schema file

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140828225231) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "campaign_feature_joins", force: true do |t|
     t.integer  "campaign_id"
     t.integer  "feature_id"
