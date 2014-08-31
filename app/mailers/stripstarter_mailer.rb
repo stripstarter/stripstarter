9ff32eeecbd33035cf53e1c97d7118316012e1c5
Stripstarter <stripstarter@gmail.com>
2014-08-31 14:22:32 -0700

    Adding initial welcome mailer

---
+++ app/mailers/stripstarter_mailer.rb
@@ -0,0 +1,8 @@
+class StripstarterMailer < ActionMailer::Base
+
+  default from: "'Stripstarter' <stripstarter@gmail.com>"
+
+  def welcome_email(user_id)
+    @user = User.find(user_id)
+    mail(to: @user.email, subject: "Welcome to Stripstarter!")
+  end
