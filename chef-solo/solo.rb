aac40234cd6c350240b9c5c0bdad2d960b8725d3
Stripstarter <stripstarter@gmail.com>
2014-09-14 17:18:16 -0700

    Commits to be checked out and refactored to work

---
+++ chef-solo/solo.rb
@@ -0,0 +1,2 @@
+file_cache_path "/Users/mzemel/Sites/stripstarter/chef-solo"
+cookbook_path ["/Users/mzemel/Sites/stripstarter/cookbooks", "/Users/mzemel/Sites/chef-repo/cookbooks", "/Users/mzemel/.berkshelf/cookbooks"]
