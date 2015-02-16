a9f90de9cdb9c0353a3af45687a67a197210a24d
Michael Zemel <michael.zemel@gmail.com>
2015-02-15 17:06:23 -0800

    Some db/schema.rb changes

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140919023013) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "campaign_feature_joins", force: true do |t|
     t.integer  "campaign_id"
     t.integer  "feature_id"
